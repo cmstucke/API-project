5f314cfcc2d56949cd2d081360af6aa1b02fe34d
cmstucke <cmstucke@gmail.com>
2023-07-12 06:59:23 -0400

    render membership association try enum input as ENUM args pt 8

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Group.hasMany(models.Membership, { foreignKey: 'groupId' })
+      // Group.hasMany(models.Membership, { foreignKey: 'groupId' })
       Group.belongsToMany(models.User, {
         through: models.Membership,
         foreignKey: 'groupId',
