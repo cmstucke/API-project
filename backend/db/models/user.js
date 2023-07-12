ba00653da86932ef49b0b7545950f9a4d40d9e8f
cmstucke <cmstucke@gmail.com>
2023-07-12 09:50:45 -0400

    get all groups by id with error pt 1

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Group, { foreignKey: 'organizerId' });
-      // User.hasMany(models.Membership, { foreignKey: 'userId' });
+      User.hasMany(models.Membership, { foreignKey: 'userId' });
       User.belongsToMany(models.Group, {
         through: models.Membership,
         foreignKey: 'userId',
--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -9,29 +9,25 @@ const { Group, User, Membership } = require('../../db/models');
 
 const router = express.Router();
 
-// Get details of a Group from an id
+// Get all Groups joined or organized by the Current User
 router.get('/current', requireAuth, async (req, res) => {
-  // console.log(req.user.id)
-  const groups = await Group.findAll({
-    where: { organizerId: req.user.id }
-  });
-  const memberships = await Group.findAll({
-    include: [{
-      model: User,
-      where: { id: req.user.id }
-    }]
-  });
-  const allGroups = groups.concat(memberships);
-  // const member = [];
-  // for (const memberOf of members) {
-  //   memberships.push(memberOf.Group)
-  // }
-  // const groups = [...organizer, ...member]
-  return res.json({ 'Groups': allGroups });
+  const organizer = await Group.findAll({ where: { organizerId: req.user.id } });
+  const memberships = await Membership.findAll({ include: { model: Group } });
+  const member = [];
+  for (const memberOf of memberships) { member.push(memberOf.Group) };
+  const groups = organizer.concat(member);
+  return res.json({ "Groups": groups });
 });
 
+// Get details of a Group from an id
 router.get('/:groupId', async (req, res) => {
   const group = await Group.findByPk(req.params.groupId);
+  if (!group) {
+    const err = new Error("Couldn't find a Group with the specified id");
+    console.error(err);
+    res.status(404);
+    return res.json({ "message": "Group couldn't be found" });
+  }
   return res.json(group);
 });
 
