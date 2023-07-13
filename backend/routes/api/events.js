db7e62290e472bc1652397ea1d2e47c998d8a2c4
cmstucke <cmstucke@gmail.com>
2023-07-13 07:37:14 -0400

    get event details by eventId pt 1

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -9,6 +9,54 @@ const { Event, Group, Venue, Attendance, EventImage } = require('../../db/models
 
 const router = express.Router();
 
+// Get details of an Event specified by its id
+router.get('/:eventId', async (req, res) => {
+  const eventId = req.params.eventId;
+  const event = await Event.findByPk(eventId, {
+    attributes: [
+      "id",
+      "groupId",
+      "venueId",
+      "name",
+      "description",
+      "type",
+      "capacity",
+      "price",
+      "startDate",
+      "endDate"
+    ],
+    include: [
+      { model: Attendance },
+      {
+        model: EventImage,
+        attributes: ['id', 'url', 'preview']
+      },
+      {
+        model: Group,
+        attributes: ['id', 'name', 'private', 'city', 'state']
+      },
+      {
+        model: Venue,
+        attributes: ['id', 'city', 'state', 'lat', 'lng']
+      },
+    ]
+  });
+
+  // No such Event
+  if (!event) {
+    const err = new Error("Couldn't find a Event with the specified id");
+    console.error(err);
+    res.status(404);
+    return res.json({ message: "Event couldn't be found" });
+  };
+
+  const eventObj = event.toJSON()
+  eventObj.numAttending = event.Attendances.length;
+  delete eventObj.Attendances;
+
+  return res.json(eventObj);
+});
+
 // Get all Events
 router.get('/', async (req, res) => {
   const events = await Event.findAll({
